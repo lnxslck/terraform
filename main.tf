e70da00032b8752b0f287cd40ef9af6faf61c979
brunoangulosolido <bruno@angulosolido.pt>
2019-10-19 01:50:57 +0100

    added_security_group

--- main.tf
+++ main.tf
@@ -60,14 +60,12 @@ resource "aws_launch_configuration" "example" {
 data "aws_availability_zones" "all" {}
 
 resource "aws_autoscaling_group" "example" {
-  launch_configuration = "${aws_launch_configuration.example.name}"
-  availability_zones = [
-    "$data.aws_availability_zones.all.names"]
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
   max_size = 10
   min_size = 2
 
-  load_balancers = [
-    aws_elb.example.name]
+  load_balancers = [aws_elb.example.name]
   health_check_type = "ELB"
 
   tag {
